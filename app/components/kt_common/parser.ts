4f5400b714b1f3edc7c182635f743332abcd3fcf
Artem Zaborskii <compartia@gmail.com>
2017-02-23 13:09:29 +0800

    sample data json parsing tools

---
+++ app/components/kt_common/parser.ts
@@ -0,0 +1,115 @@
+module kt.parser {
+
+    let fs = require('fs');
+    declare function require(name: string);
+     
+
+    function mapPOsByRefId(POs, ret = {}) {
+
+        for (let key in POs) {
+            // console.log(key);
+            let pOsByLocation = POs[key];
+            for (var value of pOsByLocation) {
+                ret[value["referenceKey"]] = new kt.graph.po_node.PONode(value);
+            }
+        }
+        return ret;
+    }
+
+    function findPOsWithAssumptions(POsByRefIdMap): kt.graph.po_node.PONode[] {
+        var has_assumptions: kt.graph.po_node.PONode[] = [];
+
+        for (var key in POsByRefIdMap) {
+            var value: kt.graph.po_node.PONode = POsByRefIdMap[key];
+            if (value.hasAssumptions()) {
+                has_assumptions.push(value)
+            }
+        }
+        return has_assumptions;
+    }
+
+    function readPoNodesFromJsons(filenames: string[]) {
+        const poByRefId = {};
+
+        for (let filename of filenames) {
+            let json = fs.readFileSync(filename, {
+                encoding: 'utf-8'
+            });
+
+            let parsed = JSON.parse(json);
+            let POs = parsed["posByKey"]["map"];
+
+            mapPOsByRefId(POs, poByRefId);
+        }
+
+        return poByRefId;
+    }
+
+    function bindChildren(poByRefId) {
+
+        const has_assumptions = findPOsWithAssumptions(poByRefId);
+        console.log("Number of POs with assumptions:" + has_assumptions.length);
+
+        let lostPoCount: number = 0;
+        for (let ppo of has_assumptions) {
+
+            for (let ref of ppo.references) {
+                const key2_: string = ref["referenceKey"]
+                let target: kt.graph.po_node.PONode = poByRefId[key2_];
+
+
+                if (!target) {
+                    console.warn(ppo.referenceKey + " refers missing key: " + key2_);
+                    target = new kt.graph.po_node.PONode(ref, true);
+                    poByRefId[key2_] = target;
+
+                    lostPoCount++;
+
+                }
+
+                ppo.addInput(target);
+                target.addOutput(ppo);
+            }
+        }
+        console.error("We have = " + lostPoCount + " missing referenceKey(s)");
+        return has_assumptions;
+    }
+
+    function buildGraph(poByRefId): tf.graph.proto.NodeDef[] {
+        let g: tf.graph.proto.NodeDef[] = [];
+
+        const has_assumptions = bindChildren(poByRefId);
+        console.log("Number of POs with assumptions:" + has_assumptions.length);
+
+        let nodesMap = {};
+
+        for (var key in poByRefId) {
+            var ppo: kt.graph.po_node.PONode = poByRefId[key];
+            if (ppo.isLinked()) {
+                if (!ppo.isTotallyDischarged())
+                    g.push(ppo.asNodeDef());
+            }
+        }
+
+        console.info["NUMBER of nodes: " + g.length];
+        return g;
+    }
+
+
+
+    export function readAndParse(): tf.graph.proto.NodeDef[] {
+
+        const ppoNodesMap = readPoNodesFromJsons([
+            "/Users/artem/work/KestrelTechnology/IN/dnsmasq/kt_analysis_export_5.6.1/src/log.c.json",
+            "/Users/artem/work/KestrelTechnology/IN/dnsmasq/kt_analysis_export_5.6.1/src/util.c.json",
+            "/Users/artem/work/KestrelTechnology/IN/dnsmasq/kt_analysis_export_5.6.1/src/cache.c.json"
+            // "/Users/artem/work/KestrelTechnology/IN/dnsmasq/kt_analysis_export_5.6.1/src/tftp.c.json"
+        ]);
+
+        let g: tf.graph.proto.NodeDef[] = buildGraph(ppoNodesMap)
+        return g;
+
+    }
+
+
+}
---
+++ app/components/kt_common/po_node.ts
@@ -0,0 +1,143 @@
+module kt.graph.po_node {
+    const SPL = "/";
+
+
+    export class PONode {
+        id: string;
+        name: string;
+        po: any;
+        predicate: string;
+        functionName: string;
+        message: string;
+        inputs: PONode[];
+        outputs: PONode[];
+        isMissing:boolean;
+
+        constructor(po, isMissing:boolean=false) {
+            this.po = po;
+            this.isMissing=isMissing;
+
+            this.inputs = [];
+            this.outputs = [];
+
+            this.predicate = po["predicateType"] ? po["predicateType"] : po["predicate"];
+            this.functionName = po["targetFuncName"] ? po["targetFuncName"] : po["functionName"];
+            this.message = po["message"] ? po["message"] : po["shortDescription"];
+            this.id = this.parseId(po["referenceKey"]);
+
+            this.name = this.makeName();
+        }
+
+        public addInput(node: PONode) {
+            this.inputs.push(node);
+        }
+        public addOutput(node: PONode) {
+            this.outputs.push(node);
+        }
+
+        get referenceKey(): string {
+            return this.po["referenceKey"];
+        }
+
+        public hasAssumptions(): boolean {
+            return this.po["references"].length > 0;
+        }
+
+        public isLinked(): boolean {
+            return this.inputs.length > 0 || this.outputs.length > 0;
+        }
+
+        get references(): any[] {
+            return this.po["references"];
+        }
+
+        private parseId(refId: string): string {
+            let s = refId.indexOf(".");
+            let ret = refId.substring(s + 1);
+            s = ret.indexOf(".");
+            ret = ret.substring(0, s);
+            return ret;
+        }
+
+        private makeName(): string {
+            let state_parent:string="";
+            if (this.isDischarged()){
+                state_parent=""+SPL + this.po["state"];
+            }
+            let _nm= this.fixFileName(this.po["file"])+ SPL + this.functionName + state_parent + SPL + this.predicate + "-" + this.id;
+            return _nm;
+        }
+
+
+        public fixFileName(file: string): string {
+            let last = file.lastIndexOf("/");
+            return file.substr(last + 1);
+        }
+
+        public isDischarged(): boolean {
+            return this.po["state"] == "DISCHARGED";
+        }
+
+        public isTotallyDischarged(): boolean {
+            if (!this.isDischarged()) {
+                return false;
+            }
+
+                for (let ref of this.inputs) {
+                    if (!ref.isDischarged()) {
+                        return false;
+                    }
+                }
+
+                for (let ref of this.outputs) {
+                    if (!ref.isDischarged()) {
+                        return false;
+                    }
+                }
+
+            return true;
+        }
+
+
+
+        public asNodeDef(): tf.graph.proto.NodeDef {
+            const po = this.po;
+
+            let ret: tf.graph.proto.NodeDef = {
+                name: this.name,
+                input: [],
+                device: po["state"],
+                op: this.functionName,
+                attr: {
+                    // "html": poRef2html(po),
+                    "predicate": this.predicate,
+                    "level": po["level"],
+                    "state": po["state"],
+                    "message": this.message
+                }
+            }
+
+            for (let ref of this.inputs) {
+                let _nm=ref.name;
+                if(ref.isMissing)
+                    _nm="^"+_nm;
+                ret.input.push(_nm);
+            }
+
+            return ret;
+        }
+
+        public toHtml(): string {
+            var html = "<div class='po level-" + this.po["level"] + " state-" + this.po["state"] + "'>"
+            // html += "<span class='func'>" + po["functionName"] + "</span><br>"
+            html += "<span class='predicate'>" + this.predicate  + " : <span class='level'> </span> </span>"
+            html += "<div class='message'>" + this.message + "</div>"
+            html += "</div>"
+
+            return html;
+        }
+
+
+
+    }
+}
