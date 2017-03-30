f26fb0924e75987cbe7f743c50c4cd746e2dc1c3
Artem Zaborskii <compartia@gmail.com>
2017-03-30 13:15:35 +0800

    "open project" feature, minor clean-ups

--- app/ts/index.ts
+++ app/ts/index.ts
@@ -1,4 +1,3 @@
-
 var graph = require('./components/tf_graph_common/lib/graph');
 var proto = require('./components/tf_graph_common/lib/proto');
 var hierarchy = require('./components/tf_graph_common/lib/hierarchy');
@@ -6,11 +5,10 @@ var render = require('./components/tf_graph_common/lib/render');
 var layout = require('./components/tf_graph_common/lib/layout');
 
 
-
-function initApp() {
-    initAppMenu();
+function initApp(): void {
+    console.info("init app");
+    kt.Globals.CWD = "/Users/artem/work/KestrelTechnology/IN/dnsmasq/ch_analysis/src/cache";
 }
 
 
-function initAppMenu() {
-}
+initApp();
