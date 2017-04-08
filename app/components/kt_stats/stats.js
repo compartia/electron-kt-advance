6d98468b2bb718d00e557e354f8162bba802b356
Artem Zaborskii <compartia@gmail.com>
2017-04-08 15:19:44 +0800

    chart

--- app/components/kt_stats/stats.js
+++ app/components/kt_stats/stats.js
@@ -7,36 +7,149 @@ var kt;
             "OPEN",
             "DISCHARGED"
         ];
+        var StatsTable = (function () {
+            function StatsTable() {
+                this.data = {};
+                this.columnNames = new Array();
+            }
+            StatsTable.prototype.inc = function (row, column, increment) {
+                var data = this.data;
+                if (!data[row]) {
+                    data[row] = {};
+                }
+                var dataset = data[row];
+                if (!dataset[column]) {
+                    dataset[column] = increment;
+                }
+                else {
+                    dataset[column] += increment;
+                }
+                if (!_.contains(this.columnNames, column)) {
+                    this.columnNames.push(column);
+                }
+            };
+            StatsTable.prototype.asNamedRowsTable = function () {
+                var rows = Object.keys(this.data);
+                var columns = this.columnNames;
+                var ret = new Array();
+                for (var _i = 0, rows_1 = rows; _i < rows_1.length; _i++) {
+                    var rowName = rows_1[_i];
+                    var row = new Array();
+                    row.push(rowName);
+                    ret.push(row);
+                    for (var _a = 0, columns_1 = columns; _a < columns_1.length; _a++) {
+                        var col = columns_1[_a];
+                        row.push(this.getAt(rowName, col));
+                    }
+                }
+                return ret;
+            };
+            Object.defineProperty(StatsTable.prototype, "summs", {
+                get: function () {
+                    return _.map(this.asTable(), function (x) { return _.sum(x); });
+                },
+                enumerable: true,
+                configurable: true
+            });
+            StatsTable.prototype.asTable = function () {
+                var rows = Object.keys(this.data);
+                var columns = this.columnNames;
+                var ret = new Array();
+                for (var _i = 0, rows_2 = rows; _i < rows_2.length; _i++) {
+                    var rowName = rows_2[_i];
+                    var row = new Array();
+                    ret.push(row);
+                    for (var _a = 0, columns_2 = columns; _a < columns_2.length; _a++) {
+                        var col = columns_2[_a];
+                        row.push(this.getAt(rowName, col));
+                    }
+                }
+                return ret;
+            };
+            StatsTable.prototype.getAt = function (row, column) {
+                return this.data[row][column];
+            };
+            return StatsTable;
+        }());
+        stats.StatsTable = StatsTable;
         var Stats = (function () {
             function Stats() {
             }
             Stats.prototype.build = function (project) {
                 console.info("building statistics");
-                this.byPredicate = {};
+                this.byPredicate = new StatsTable();
                 this._predicates = _.uniq(_.map(project.proofObligations, function (e) { return e.predicate; })).sort();
                 for (var _i = 0, states_1 = states; _i < states_1.length; _i++) {
                     var state = states_1[_i];
                     for (var _a = 0, _b = this._predicates; _a < _b.length; _a++) {
                         var predicate = _b[_a];
-                        this.inc(this.byPredicate, state, predicate, 0);
+                        this.byPredicate.inc(predicate, state, 0);
                     }
                 }
                 for (var _c = 0, _d = project.filteredProofObligations; _c < _d.length; _c++) {
                     var po = _d[_c];
-                    this.inc(this.byPredicate, po.state, po.predicate, 1);
+                    this.byPredicate.inc(po.predicate, po.state, 1);
                 }
             };
-            Stats.prototype.inc = function (data, datasetKey, key, increment) {
-                if (!data[datasetKey]) {
-                    data[datasetKey] = {};
-                }
-                var dataset = data[datasetKey];
-                if (!dataset[key]) {
-                    dataset[key] = increment;
-                }
-                else {
-                    dataset[key] += increment;
-                }
+            Stats.prototype.updateChart = function (container) {
+                var t = d3.transition()
+                    .duration(750);
+                var table = this.byPredicate.asNamedRowsTable();
+                var data = _.map(table, function (x) {
+                    return {
+                        "values": x.splice(1),
+                        "name": x[0]
+                    };
+                });
+                var summs = this.byPredicate.summs;
+                var max = _.max(summs);
+                var widthFunc = function (val, col) {
+                    var ret = Math.round(10000 * (val / max)) / 100.1;
+                    console.info(ret);
+                    return ret;
+                };
+                var bgFunc = function (val, index) {
+                    return "var(--kt-state-" + states[index].toLowerCase() + "-default-bg)";
+                };
+                var ident = function (d) { return d; };
+                var rowname = function (x) { return x["name"]; };
+                var rowvalues = function (x) { return x["values"]; };
+                var setupBar = function (element) {
+                    element
+                        .style("background-color", bgFunc)
+                        .attr("class", "bar")
+                        .style("width", function (val, index) { return widthFunc(val, index) + "%"; })
+                        .style("display", function (val, index) { return widthFunc(val, index) > 0 ? "block" : "none"; })
+                        .attr("title", ident);
+                };
+                var rows = container.selectAll(".row").data(data, function (d) { return d.name; });
+                var newRows = rows.enter()
+                    .append("div").attr("class", "row");
+                newRows.append("label")
+                    .text(rowname)
+                    .attr("title", rowname);
+                var cells_in_new_rows = newRows.append("div")
+                    .attr("class", "bar-container");
+                var cells = rows.select(".bar-container");
+                var updateBars = function (bars) {
+                    setupBar(bars);
+                    setupBar(bars.enter()
+                        .append("a")
+                        .style('opacity', 0.0)
+                        .transition()
+                        .delay(10)
+                        .duration(500)
+                        .style('opacity', 1.0));
+                    bars.exit().remove();
+                };
+                updateBars(cells_in_new_rows.selectAll("a").data(rowvalues));
+                updateBars(cells.selectAll("a").data(rowvalues));
+                rows.exit()
+                    .transition()
+                    .delay(10)
+                    .duration(500)
+                    .style('opacity', 0.0)
+                    .remove();
             };
             Object.defineProperty(Stats.prototype, "chartDataPredicatesByState", {
                 get: function () {
--- app/components/kt_stats/stats.ts
+++ app/components/kt_stats/stats.ts
@@ -7,41 +7,210 @@ module kt.stats {
         "OPEN",
         "DISCHARGED"
     ];
-    
+
+
+    export class StatsTable {
+        data: { [key: string]: { [key: string]: number } } = {};
+
+        columnNames: Array<string> = new Array();
+
+        public inc(row: string, column: string, increment: number) {
+
+            let data = this.data;
+
+            if (!data[row]) {
+                data[row] = {};
+            }
+            let dataset = data[row];
+            if (!dataset[column]) {
+                dataset[column] = increment;
+            } else {
+                dataset[column] += increment;
+            }
+
+            if (!_.contains(this.columnNames, column)) {
+                this.columnNames.push(column);
+            }
+        }
+
+        /**
+        example:
+        [["row1name", 3,2,3,]
+        ["row2name", 4,2,3,]
+        ["row3name", 5,2,3,]
+        ]
+        */
+        public asNamedRowsTable(): Array<Array<string | number>> {
+            let rows = Object.keys(this.data);
+            let columns = this.columnNames;
+
+            let ret = new Array<Array<string | number>>();
+
+            for (let rowName of rows) {
+                let row = new Array<string | number>();
+                row.push(rowName);
+                ret.push(row);
+                for (let col of columns) {
+                    row.push(this.getAt(rowName, col));
+                }
+            }
+
+            return ret;
+        }
+
+        /**
+            returns a vector Vi, where each value is a summ of row values
+        */
+        get summs(): Array<number> {
+            return _.map(this.asTable(), (x) => _.sum(x));
+        }
+
+        public asTable(): Array<Array<number>> {
+            let rows = Object.keys(this.data);
+            let columns = this.columnNames;
+
+            let ret = new Array<Array<string | number>>();
+
+            for (let rowName of rows) {
+                let row = new Array<string | number>();
+                ret.push(row);
+                for (let col of columns) {
+                    row.push(this.getAt(rowName, col));
+                }
+            }
+
+            return ret;
+        }
+
+        public getAt(row: string, column: string): number {
+            return this.data[row][column];
+        }
+    }
+
     export class Stats {
 
-        byPredicate: {};
+        byPredicate: StatsTable;
         _predicates: Array<any>;
 
         public build(project: kt.Globals.Project) {
             console.info("building statistics");
-            this.byPredicate = {};
+            this.byPredicate = new StatsTable();
 
             this._predicates = _.uniq(_.map(project.proofObligations, (e) => e.predicate)).sort();
 
+            //popolate with zeros
             for (let state of states) {
                 for (let predicate of this._predicates) {
-                    this.inc(this.byPredicate, state, predicate, 0);
+                    this.byPredicate.inc(predicate, state, 0);
                 }
             }
 
             for (let po of project.filteredProofObligations) {
-                this.inc(this.byPredicate, po.state, po.predicate, 1);
+                this.byPredicate.inc(po.predicate, po.state, 1);
             }
 
         }
 
-        private inc(data: any, datasetKey: string, key: string, increment: number) {
-            if (!data[datasetKey]) {
-                data[datasetKey] = {};
+
+
+        public updateChart(container: d3.Selection<any>) {
+            const t = d3.transition()
+                .duration(750);
+
+            const table = this.byPredicate.asNamedRowsTable();
+            const data = _.map(table, (x) => {
+                return {
+                    "values": x.splice(1),
+                    "name": x[0]
+                }
+            });
+            const summs = this.byPredicate.summs;
+            const max = _.max(summs);
+
+            const widthFunc = (val, col) => {
+                let ret = Math.round(10000 * (val / max)) / 100.1;
+                console.info(ret);
+                return ret;
             }
-            let dataset = data[datasetKey];
-            if (!dataset[key]) {
-                dataset[key] = increment;
-            } else {
-                dataset[key] += increment;
+
+            const bgFunc = (val, index) => {
+                return "var(--kt-state-" + states[index].toLowerCase() + "-default-bg)"
             }
+
+            const ident = (d) => d;
+
+            const rowname = (x) => x["name"];
+            const rowvalues = (x) => x["values"];
+
+
+            const setupBar = (element) => {
+                element
+                    .style("background-color", bgFunc)
+                    .attr("class", "bar")
+                    .style("width", (val, index) => widthFunc(val, index) + "%")
+                    .style("display", (val, index) => widthFunc(val, index) > 0 ? "block" : "none")
+                    .attr("title", ident);
+            }
+
+            //=============
+
+            let rows = container.selectAll(".row").data(data, (d) => d.name);
+
+
+            let newRows = rows.enter()
+                .append("div").attr("class", "row");
+
+            newRows.append("label")
+                .text(rowname)
+                .attr("title", rowname);
+
+
+
+            let cells_in_new_rows = newRows.append("div")
+                .attr("class", "bar-container");
+
+
+            let cells = rows.select(".bar-container");
+
+            let updateBars = (bars) => {
+                /** UPDATE*/
+                setupBar(bars);
+
+                /** ENTER*/
+                setupBar(
+                    bars.enter()
+                        .append("a")
+                        .style('opacity', 0.0)
+                        .transition()
+                        .delay(10)
+                        .duration(500)
+                        .style('opacity', 1.0));
+
+                /** EXIT*/
+                bars.exit().remove();
+            }
+
+            updateBars(cells_in_new_rows.selectAll("a").data(rowvalues));
+            updateBars(cells.selectAll("a").data(rowvalues));
+
+
+            //
+            rows.exit()
+                .transition()
+                .delay(10)
+                .duration(500)
+                .style('opacity', 0.0)
+                .remove();
+
+
         }
+
+
+
+
+
+
+
 
         get chartDataPredicatesByState() {
             let ret = [];
