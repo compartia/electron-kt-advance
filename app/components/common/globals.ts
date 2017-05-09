3d6105add6e22edf8f95e56a25f27d403bebba1b
Artem Zaborskii <compartia@gmail.com>
2017-05-09 23:09:37 +0800

    show in-source expandable proof obligations :seedling:

--- app/components/common/globals.ts
+++ app/components/common/globals.ts
@@ -49,203 +49,6 @@ module kt.Globals {
 
     }
 
-    export class Filter {
-
-        private _predicates: util.StringSet = new util.StringSet([]);
-        private _states: util.AnySet<model.PoStates> = new util.AnySet<model.PoStates>([]);
-        private _levels: util.StringSet = new util.StringSet([]);
-        private _dischargeTypes: util.StringSet = new util.StringSet([]);
-
-
-        private _cfunction: xml.CFunction;
-        private _file: treeview.FileInfo;
-        private _line: number = null;
-
-        set line(line: number) {
-            this._line = line;
-        }
-
-        get line(): number {
-            if (this._line != null && this._line >= 0) {
-                return this._line;
-            } else {
-                if (this.cfunction) {
-                    return this.cfunction.line;
-                }
-            }
-            return 0;
-        }
-
-
-        get file(): treeview.FileInfo {
-            return this._file;
-        }
-
-        get predicates(): util.StringSet {
-            return this._predicates;
-        }
-
-        get singlePredicate(): string {
-            if (this._predicates) {
-                if (this._predicates.length == 1) {
-                    return this._predicates.first;
-                }
-                else if (this._predicates.length > 1) {
-                    return undefined;
-                }
-            }
-            return null;
-        }
-
-        get singleState(): model.PoStates {
-            if (this._states) {
-                if (this._states.length == 1) {
-                    return this._states.first;
-                }
-                else if (this._predicates.length > 1) {
-                    return undefined;
-                }
-            }
-            return null;
-        }
-
-        set predicates(_predicates: util.StringSet) {
-            this._predicates = _predicates;
-        }
-
-        set dischargeTypes(newDischargeTypes: util.StringSet) {
-            this._dischargeTypes = newDischargeTypes;
-        }
-
-        get dischargeTypes(): util.StringSet {
-            return this._dischargeTypes;
-        }
-
-        set states(_states: util.AnySet<model.PoStates>) {
-            this._states = _states;
-        }
-
-        get states(): util.AnySet<model.PoStates> {
-            return this._states;
-        }
-
-
-        set levels(_levels: util.StringSet) {
-            this._levels = _levels;
-        }
-
-        get levels(): util.StringSet {
-            return this._levels;
-        }
-
-
-
-        public reset() {
-            this._cfunction = null;
-            this._file = null;
-            this._states.values = model.PoStatesArr;
-            this._dischargeTypes.values = model.PoDischargeTypesArr;
-        }
-
-        set cfunction(_cfunction: xml.CFunction) {
-            this._line = null;
-            if (_cfunction) {
-                this.file = _cfunction.fileInfo;
-            }
-            this._cfunction = _cfunction;
-        }
-
-        get cfunction() {
-            return this._cfunction;
-        }
-
-        set state(_state: model.PoStates) {
-            this._states.values = [_state];
-        }
-
-        get fileName() {
-            if (this._file)
-                return this._file.relativePath;
-            return null;
-        }
-
-
-        set file(file: treeview.FileInfo) {
-            if ((file && this._file) || (!file)) {
-                if (file.relativePath != this._file.relativePath) {
-                    this._cfunction = null;
-                    this._line = null;
-                }
-            }
-            this._file = file;
-        }
-
-
-
-
-        private acceptFile(po: model.AbstractNode): boolean {
-            if (!this.fileName) {
-                return true;
-            } else {
-                if (!this._file.dir) {
-                    return po.file == this.fileName;
-                } else {
-                    return po.file.startsWith(this.fileName) || this.fileName == ".";
-                }
-
-            }
-        }
-
-
-
-        private acceptFunction(po: model.AbstractNode): boolean {
-            if (!this.cfunction) {
-                return true;
-            } else {
-                return po.functionName == this.cfunction.name;//XXX: compare file
-            }
-        }
-
-        private acceptState(po: model.ProofObligation): boolean {
-            if (this.states == null || this._states.contains(po.state)) {
-                return true;
-            }
-            return false;
-        }
-
-        private acceptLevel(po: model.AbstractNode): boolean {
-            return this.levels.contains((<model.ProofObligation>po).level);
-        }
-
-        private acceptDischargeType(po: model.ProofObligation): boolean {
-            if (!po.dischargeType) {
-                return this._dischargeTypes.contains("default");
-            } else {
-                if (this._dischargeTypes == null || this._dischargeTypes.contains(po.dischargeType.toLowerCase())) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-
-        private acceptPredicate(po: model.ProofObligation): boolean {
-            if (this._predicates == null || this._predicates.contains(po.predicate.toLowerCase())) {
-                return true;
-            }
-            return false;
-        }
-
-        public accept(po: model.ProofObligation): boolean {
-            return this.acceptState(po) && this.acceptLevel(po) && this.acceptFile(po) && this.acceptFunction(po) && this.acceptPredicate(po) && this.acceptDischargeType(po);
-        }
-
-        public acceptApi(po: model.ApiNode): boolean {
-            return this.acceptFile(po) && this.acceptFunction(po);// && this.acceptPredicate(po) && this.acceptState(po) && this.acceptDischargeType(po);
-        }
-
-    }
 
     /**
     @deprecated;
@@ -254,8 +57,9 @@ module kt.Globals {
     export interface FileContents {
         src: string;
     }
+
     export class Project {
-        functionByFile: { [key: string]: Array<kt.xml.CFunction> } = {};
+        functionByFile: { [key: string]: Array<xml.CFunction> } = {};
         baseDir: string;
         analysisDir: string;
         stats: stats.Stats;
@@ -272,13 +76,22 @@ module kt.Globals {
             this.baseDir = baseDir;
         }
 
+        public getPosAtLine(fileName: string, line: number): Array<model.ProofObligation> {
+            let ret = new Array<model.ProofObligation>();
+            for (let po of this.filteredProofObligations) {
+                if (po.location.line == line && po.file == fileName) {
+                    ret.push(po);
+                }
+            }
+            return ret;
+        }
+
         public loadFile(relativePath: string): Promise<FileContents> {
             let self = this;
             let filename = path.join(this.baseDir, relativePath);
             console.info("reading " + filename);
 
             return new Promise((resolve, reject) => {
-
 
                 fs.readFile(filename, 'utf8', (err, data: string) => {
                     if (err) {
@@ -292,26 +105,21 @@ module kt.Globals {
                     }
                     // data is the contents of the text file we just read
                 });
-
-
-
             });
-
         }
 
         private parseSourceFile(contents: string) {
+            //XXX: types!!
             let lines = contents.split(/\r\n|\r|\n/g);
             let ret = [];
             for (let line of lines) {
                 ret.push({
                     text: line,
                     stats: {
-                        violations: 34,
-                        open: 34,
+                        violations: 0,
+                        open: 0,
                     }
-                }
-
-                );
+                });
             }
             return ret;
         }
