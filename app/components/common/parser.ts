7add65d95c1587d01ef3fc465c29a0004dee28f4
Artem Zaborskii <compartia@gmail.com>
2017-05-10 18:36:41 +0800

    minor code clean-up

--- app/components/common/parser.ts
+++ app/components/common/parser.ts
@@ -7,8 +7,6 @@ module kt.parser {
 
         let nodesMap = {};
 
-
-
         for (let ppo of pos) {
             if (ppo.isLinked()) {
                 let node: tf.graph.proto.NodeDef = ppo.asNodeDef(filter);
@@ -24,14 +22,13 @@ module kt.parser {
         }
 
 
-
         console.info["NUMBER of nodes: " + g.length];
         return g;
     }
 
 
 
-    export function readAndParse(project: Globals.Project, tracker: tf.ProgressTracker): Promise<kt.Globals.Project> {
+    export function readAndParse(project: Globals.Project, tracker: tf.ProgressTracker): Promise<Globals.Project> {
 
         let reader: xml.XmlReader = new xml.XmlReader();
 
@@ -44,19 +41,14 @@ module kt.parser {
         return reader.readFunctionsMap(path.dirname(project.analysisDir), readFunctionsMapTracker)
             .then(functions => {
 
-                // let byNameMap = _.indexBy(funcs, 'name');
-                // console.info("total functions: " + funcs.length + " \t\ttotal unique keys: " + Object.keys(byNameMap).length);
-                // // console.info(byNameMap);
 
                 let resultingMap: { [key: string]: Array<treeview.FileInfo> } = {};
-
 
                 for (let f of functions) {
                     if (!resultingMap[f.name]) {
                         resultingMap[f.name] = [];
                     }
                     resultingMap[f.name].push(f.fileInfo);
-
                 }
 
                 project.functionByFile = reader.buildFunctionsByFileMap(functions);
@@ -66,9 +58,6 @@ module kt.parser {
             .then((POs: xml.XmlAnalysis) => {
                 project.proofObligations = model.sortPoNodes(POs.ppos.concat(POs.spos));
                 project.apis = POs.apis;
-
-                // let g: tf.graph.proto.NodeDef[] = buildGraph(project.proofObligations, project.apis);
-                // return g;
 
                 return project;
             });
