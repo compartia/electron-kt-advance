3c6921660b08dd66bc897b5c670ad76dc83302a6
Artem Zaborskiy <compartia@gmail.com>
2018-08-03 23:15:17 +0300

    Test

--- electron-builder-afterPack.js
+++ electron-builder-afterPack.js
@@ -2,4 +2,6 @@
 exports.default = async function(context) {
     console.error("CSC_LINK ----------------------------------------------");
     console.error(process.env.CSC_LINK);
+    console.error("__TEST="+process.env.__TEST);
+    
 }
